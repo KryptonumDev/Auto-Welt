c8b133da9a427a825e2ae9af29a423a55afdf9bd
Albert Strzyzewski <albert.strzyzewski@reckitt.com>
2022-09-27 22:59:31 +0200

    Dodanie layoutu i SEO

---
+++ src/components/Layouts/GlobalLayout.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { MenuProvider } from "../../context/menuContext"
+import GlobalStyle from "../../styles/GlobalStyle"
+import SEO from "../../seo/seo"
+
+const GlobalLayout = ({ children }) => {
+  return (
+    <>
+      <MenuProvider>
+        <GlobalStyle />
+        <SEO />
+        {children}
+      </MenuProvider>
+    </>
+  )
+}
+
+export default GlobalLayout
