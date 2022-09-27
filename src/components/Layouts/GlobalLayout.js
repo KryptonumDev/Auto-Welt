35b14534b68ac3dabf3ded0a92dd13fcf4cf401b
Albert Strzyzewski <albert.strzyzewski@reckitt.com>
2022-09-27 23:44:58 +0200

    Chwilowe usunięcie SEO

--- src/components/Layouts/GlobalLayout.js
+++ src/components/Layouts/GlobalLayout.js
@@ -1,14 +1,12 @@
 import React from "react"
 import { MenuProvider } from "../../context/menuContext"
 import GlobalStyle from "../../styles/GlobalStyle"
-// import SEO from "../SEO/SEO"
 
 const GlobalLayout = ({ children }) => {
   return (
     <>
       <MenuProvider>
         <GlobalStyle />
-        {/* <SEO /> */}
         {children}
       </MenuProvider>
     </>
