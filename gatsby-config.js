8b4261b3490c7c70ed55cef1eeb07321aeddfe06
Albert Strzyzewski <albert.strzyzewski@reckitt.com>
2022-09-27 22:01:32 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `auto-welt`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+};
