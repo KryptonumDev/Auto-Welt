1b6df4120a510c5f8ee58f85c3de12b8b5be89dc
Albert Strzyzewski <albert.strzyzewski@reckitt.com>
2022-09-28 02:53:53 +0200

    Podstawowe style dla hamburger buttona

---
+++ src/components/HamburgerButton/HamburgerButton.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { StyledHamburgerButton } from "./StyledHamburgerButton"
+
+const HamburgerButton = () => {
+    return (
+        <StyledHamburgerButton>
+            button
+        </StyledHamburgerButton>
+    )
+}
+
+export default HamburgerButton
---
+++ src/components/HamburgerButton/StyledHamburgerButton.js
@@ -0,0 +1,3 @@
+import styled from "styled-components"
+
+export const StyledHamburgerButton = styled.button``
