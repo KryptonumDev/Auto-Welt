7af922ca28a5445efff8e7ae6642bb0a5445223d
Albert Strzyzewski <albert.strzyzewski@reckitt.com>
2022-09-29 03:49:02 +0200

    Naprawa buga z fontem

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         web: [
           {
             name: "Roboto",
-            file: "https://fonts.googleapis.com/css2?family=Roboto:400,500,600,700",
+            file: "https://fonts.googleapis.com/css2?family=Roboto",
           },
           {
             name: "Nocturne Serif",
