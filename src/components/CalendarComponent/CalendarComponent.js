fdeacbcde114c1a9760be6e01a46dcf51ea4f676
CodeYouLike <strzyzewski.albertms@gmail.com>
2022-10-01 23:35:48 +0200

    Basic calendar component

--- src/components/CalendarComponent/CalendarComponent.js
+++ src/components/CalendarComponent/CalendarComponent.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { StyledCalendarComponent } from "./StyledCalendarComponent";
+import { StyledCalendarComponent, StyledCalendar } from "./StyledCalendarComponent";
 
 const CalendarComponent = () => {
-    return <StyledCalendarComponent></StyledCalendarComponent>;
+    const [value, onChange] = useState(new Date());
+
+    return (
+        <StyledCalendarComponent>
+            <StyledCalendar 
+                onChange={onChange} 
+                value={value} 
+                defaultValue={[
+                    new Date(2022, 10, 1), 
+                    new Date(2022, 10, 2), 
+                    new Date(2022, 10, 3)
+                ]}
+                
+                defaultView="month"
+                formatShortWeekday={(locale, date) => {
+                    switch (date.getDay()) {
+                      case 0:
+                        return "NIEDZIELA";
+                      case 1:
+                        return "PONIEDZIAŁEK";
+                      case 2:
+                        return "WTOREK";
+                      case 3:
+                        return "ŚRODA";
+                      case 4:
+                        return "CZWARTE";
+                      case 5:
+                        return "PIĄTEK";
+                      case 6:
+                        return "SOBOTA";
+                    }
+                }}
+                tileContent={({ activeStartDate, date, view }) => view === 'month' && date.getDay() === 0 ? <p>It's Sunday!</p> : null}
+                navigationLabel={({ date, label, locale, view }) => `Current view: ${view}, date: ${date.toLocaleDateString(locale)}`}
+            />
+        </StyledCalendarComponent>
+    );
 }
 
 export default CalendarComponent
--- src/components/CalendarComponent/StyledCalendarComponent.js
+++ src/components/CalendarComponent/StyledCalendarComponent.js
@@ -1,3 +1,7 @@
 import styled from "styled-components"
-
+import Calendar from 'react-calendar' 
 export const StyledCalendarComponent = styled.div``
+
+export const StyledCalendar = styled(Calendar)`
+
+`
