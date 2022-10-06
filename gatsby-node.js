87b9c00cbe37d408f9af2949134924abb57ead06
CodeYouLike <strzyzewski.albertms@gmail.com>
2022-10-07 00:27:46 +0200

    Dynamiczne tworzenie kolekcji

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+exports.createPages = async ({ actions, graphql }) => {
+    const { createPage } = actions
+
+    const kolekcjeData = await graphql(`
+        {
+            allWpKolekcje {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `)
+
+    kolekcjeData.data.allWpKolekcje.edges.map(({ node }) => {
+        createPage({
+            path: `kolekcje/${node.slug}`,
+            component: require.resolve("./src/components/Templates/CollectionTemplate/CollectionTemplate.js"),
+            context: {
+                kolekcjaId: node.id
+            },
+        })
+    })
+}
