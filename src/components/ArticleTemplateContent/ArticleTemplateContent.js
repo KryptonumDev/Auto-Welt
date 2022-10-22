43b06fd21cb38e178414156f9a9859548bc7564e
AsD Monio <asdmonio@gmail.com>
2022-10-22 15:13:21 +0200

    ArticleTemplateContent: Generuj spis treści z nagłówków w treści

--- src/components/ArticleTemplateContent/ArticleTemplateContent.js
+++ src/components/ArticleTemplateContent/ArticleTemplateContent.js
@@ -14,29 +14,56 @@ import {
 } from "./StyledArticleTemplateContent";
 
 const ArticleTemplateContent = ({ contentData }) => {
+  const content = parse(contentData.content, {
+    replace: (domNode) => {
+      if (domNode.name === "blockquote")
+        return (
+          <ArticleCustomQuote
+            quoteText={
+              domNode.children
+              .find(
+                child => child.name === "cite"
+              )
+              ?.children?.[0]
+              ?.data
+            }
+          />
+        );
+    }
+  }),
+    headers = (() => {
+      const nav = [],
+        newHeaderObj = (name = "", children = []) => ({ name, children })
+      for (let header of content.filter(val => [ "h2", "h3" ].includes(val.type))) {
+        const headerContent = (
+          Array.isArray(header.props.children) ? (
+            header.props.children?.[0]
+          ) : (
+            header.props.children
+          )
+        ), headerObj = header.type === "h2" ? newHeaderObj(headerContent) : nav.pop();
+
+        if (!headerObj) {
+          console.warn("ArticleTemplateContent", "Got 'h3' before 'h2'! Aborting!");
+          return undefined;
+        }
+
+        if (header.type === "h3")
+          headerObj.children.push(newHeaderObj(headerContent));
+        nav.push(headerObj);
+      }
+      return nav;
+    })();
+
   return (
     <StyledArticleTemplateContent>
       <StyledAside>
-        <CustomAside asideData={contentData.artykul.dedykowanaStronaArtykulu.sekcjaPolecajacaNaszeKolekcjeWLewejCzesciStrony} />
+        <CustomAside
+          asideData={contentData.artykul.dedykowanaStronaArtykulu.sekcjaPolecajacaNaszeKolekcjeWLewejCzesciStrony}
+        />
       </StyledAside>
       <StyledTextContent>
-        {parse(contentData.content, {
-          replace: (domNode) => {
-            if (domNode.name === "blockquote")
-              return (
-                <ArticleCustomQuote
-                  quoteText={
-                    domNode.children
-                    .find(
-                      child => child.name === "cite"
-                    )
-                    ?.children?.[0]
-                    ?.data
-                  }
-                />
-              );
-          }
-        })}
+        {content}
         <ArticleCustomQuote quoteText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. " />
         <ArticleGalleryImage images={contentData.artykul.dedykowanaStronaArtykulu?.galeriaNaKoncuArtykulu}/>
         <ArticlePhotoInfo desc={contentData.artykul.dedykowanaStronaArtykulu.sekcjaZAutoremZdjec?.opis} />
