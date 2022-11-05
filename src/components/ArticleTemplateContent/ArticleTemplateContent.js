644c6ba2c07a6a0b4d519a436189c7b5d712db24
Albert Strzyzewski <albert.strzyzewski@rb.com>
2022-11-05 23:46:38 +0100

    Format prettier

--- src/components/ArticleTemplateContent/ArticleTemplateContent.js
+++ src/components/ArticleTemplateContent/ArticleTemplateContent.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import parse from "html-react-parser";
 
@@ -8,120 +10,127 @@ import ArticlePhotoInfo from "../ArticlePhotoInfo/ArticlePhotoInfo";
 import ArticleCustomQuote from "../ArticleCustomQuote/ArticleCustomQuote";
 
 import {
-  StyledArticleTemplateContent,
-  StyledAside,
-  StyledTextContent,
+    StyledArticleTemplateContent,
+    StyledAside,
+    StyledTextContent,
 } from "./StyledArticleTemplateContent";
 
 const ArticleTemplateContent = ({ contentData, slug }) => {
-  let headerIds = {};
-  const content = contentData.content
-      ? parse(contentData?.content, {
-          replace: (domNode) => {
-            if (!domNode.children) return;
+    let headerIds = {};
+    const content = contentData.content
+            ? parse(contentData?.content, {
+                  replace: (domNode) => {
+                      if (!domNode.children) return;
 
-            switch (domNode.name) {
-              case "blockquote":
-                return (
-                  <ArticleCustomQuote
-                    quoteText={
-                      domNode.children.find((child) => child.name === "cite")
-                        ?.children?.[0]?.data
+                      switch (domNode.name) {
+                          case "blockquote":
+                              return (
+                                  <ArticleCustomQuote
+                                      quoteText={
+                                          domNode.children.find(
+                                              (child) => child.name === "cite"
+                                          )?.children?.[0]?.data
+                                      }
+                                  />
+                              );
+                          case "h2":
+                          case "h3":
+                              const id = domNode.children[0].data
+                                  .replace(" ", "_")
+                                  .normalize("NFKD")
+                                  .replace(/\u0142/g, "l")
+                                  .replace(/[^\w]/g, "")
+                                  .replace("_", "-")
+                                  .toLowerCase();
+                              headerIds[id] = (headerIds[id] ?? 0) + 1;
+                              domNode.attribs.id = `${id}-${headerIds[id]}`;
+                      }
+                  },
+              })
+            : null,
+        headers = (() => {
+            const nav = [],
+                newHeaderObj = (id, name = "", children = []) => ({
+                    id,
+                    name,
+                    children,
+                });
+            for (let header of content?.filter((val) =>
+                ["h2", "h3"].includes(val.type)
+            )) {
+                const headerContent = Array.isArray(header.props.children)
+                        ? header.props.children?.[0]
+                        : header.props.children,
+                    { id } = header.props,
+                    headerObj =
+                        header.type === "h2"
+                            ? newHeaderObj(id, headerContent)
+                            : nav.pop();
+
+                if (!headerObj) {
+                    console.warn(
+                        "ArticleTemplateContent",
+                        "Got 'h3' before 'h2'! Aborting!"
+                    );
+                    return undefined;
+                }
+
+                if (header.type === "h3")
+                    headerObj.children.push(newHeaderObj(id, headerContent));
+                nav.push(headerObj);
+            }
+            return nav;
+        })();
+
+    return (
+        <StyledArticleTemplateContent>
+            <StyledAside>
+                <CustomAside
+                    tableOfContents={headers}
+                    asideData={
+                        contentData?.artykul?.dedykowanaStronaArtykulu
+                            ?.sekcjaPolecajacaNaszeKolekcjeWLewejCzesciStrony
                     }
-                  />
-                );
-              case "h2":
-              case "h3":
-                const id = domNode.children[0].data
-                  .replace(" ", "_")
-                  .normalize("NFKD")
-                  .replace(/\u0142/g, "l")
-                  .replace(/[^\w]/g, "")
-                  .replace("_", "-")
-                  .toLowerCase();
-                headerIds[id] = (headerIds[id] ?? 0) + 1;
-                domNode.attribs.id = `${id}-${headerIds[id]}`;
-            }
-          },
-        })
-      : null,
-    headers = (() => {
-      const nav = [],
-        newHeaderObj = (id, name = "", children = []) => ({
-          id,
-          name,
-          children,
-        });
-      for (let header of content?.filter((val) =>
-        ["h2", "h3"].includes(val.type)
-      )) {
-        const headerContent = Array.isArray(header.props.children)
-            ? header.props.children?.[0]
-            : header.props.children,
-          { id } = header.props,
-          headerObj =
-            header.type === "h2" ? newHeaderObj(id, headerContent) : nav.pop();
-
-        if (!headerObj) {
-          console.warn(
-            "ArticleTemplateContent",
-            "Got 'h3' before 'h2'! Aborting!"
-          );
-          return undefined;
-        }
-
-        if (header.type === "h3")
-          headerObj.children.push(newHeaderObj(id, headerContent));
-        nav.push(headerObj);
-      }
-      return nav;
-    })();
-
-  return (
-    <StyledArticleTemplateContent>
-      <StyledAside>
-        <CustomAside
-          tableOfContents={headers}
-          asideData={
-            contentData?.artykul?.dedykowanaStronaArtykulu
-              ?.sekcjaPolecajacaNaszeKolekcjeWLewejCzesciStrony
-          }
-        />
-      </StyledAside>
-      <StyledTextContent>
-        {content}
-        {contentData.artykul?.dedykowanaStronaArtykulu
-          ?.galeriaNaKoncuArtykulu && (
-          <ArticleGalleryImage
-            images={
-              contentData.artykul.dedykowanaStronaArtykulu
-                ?.galeriaNaKoncuArtykulu
-            }
-          />
-        )}
-        {contentData.artykul?.dedykowanaStronaArtykulu?.sekcjaZAutoremZdjec
-          ?.opis && (
-          <ArticlePhotoInfo
-            desc={
-              contentData.artykul?.dedykowanaStronaArtykulu?.sekcjaZAutoremZdjec
-                ?.opis
-            }
-            link={contentData.artykul?.dedykowanaStronaArtykulu?.sekcjaZAutoremZdjec
-              ?.linkDoStrony}
-          />
-        )}
-        {contentData.artykul?.dedykowanaStronaArtykulu
-          ?.ktoryArtykulPolecicNaDoleStrony.slug === slug ? null : (
-          <ChooseArticle
-            chosenArticle={
-              contentData.artykul?.dedykowanaStronaArtykulu
-                ?.ktoryArtykulPolecicNaDoleStrony
-            }
-          />
-        )}
-      </StyledTextContent>
-    </StyledArticleTemplateContent>
-  );
+                />
+            </StyledAside>
+            <StyledTextContent>
+                {content}
+                {contentData.artykul?.dedykowanaStronaArtykulu
+                    ?.galeriaNaKoncuArtykulu && (
+                    <ArticleGalleryImage
+                        images={
+                            contentData.artykul.dedykowanaStronaArtykulu
+                                ?.galeriaNaKoncuArtykulu
+                        }
+                    />
+                )}
+                {contentData.artykul?.dedykowanaStronaArtykulu
+                    ?.sekcjaZAutoremZdjec?.opis && (
+                    <ArticlePhotoInfo
+                        desc={
+                            contentData.artykul?.dedykowanaStronaArtykulu
+                                ?.sekcjaZAutoremZdjec?.opis
+                        }
+                        link={
+                            contentData.artykul?.dedykowanaStronaArtykulu
+                                ?.sekcjaZAutoremZdjec?.linkDoStrony
+                        }
+                    />
+                )}
+                {contentData.artykul?.dedykowanaStronaArtykulu
+                    ?.ktoryArtykulPolecicNaDoleStrony.slug ===
+                slug ? null : contentData.artykul?.dedykowanaStronaArtykulu
+                      ?.ktoryArtykulPolecicNaDoleStrony ? (
+                    <ChooseArticle
+                        chosenArticle={
+                            contentData.artykul?.dedykowanaStronaArtykulu
+                                ?.ktoryArtykulPolecicNaDoleStrony
+                        }
+                    />
+                ) : null}
+            </StyledTextContent>
+        </StyledArticleTemplateContent>
+    );
 };
 
 export default ArticleTemplateContent;
