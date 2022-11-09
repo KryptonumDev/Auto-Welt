5e621fa93fb5d9e8751829845bcce0b0de93fe36
Albert Strzyzewski <albert.strzyzewski@rb.com>
2022-11-10 00:34:35 +0100

    zmiana jakosci zdjec

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
                 defaults: {
                     formats: [`auto`, `webp`],
                     placeholder: `blurred`,
-                    quality: 100,
+                    quality: 90,
                 },
             },
         },
