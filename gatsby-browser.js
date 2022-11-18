d32344c733a8980ab18c665915f10da44a278902
kryptonumstudio <kryptonumstudio@gmail.com>
2022-11-18 13:30:35 +0100

    disable loader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 const React = require("react");
 const GlobalLayout = require("./src/components/Layouts/GlobalLayout").default;
 const AnimatePresence = require("framer-motion").AnimatePresence;
-const gsap = require("gsap").gsap;
+// const gsap = require("gsap").gsap;
 
 
 exports.wrapPageElement = ({ element, props }) => {
@@ -12,10 +12,10 @@ exports.wrapPageElement = ({ element, props }) => {
   );
 };
 
-exports.onClientEntry = () => {
-  window.addEventListener("load", () => {
-    const body = document.querySelector("body");
-    body.className = document.body.className.replace(/\bno-js\b/, "");
-    gsap.from(body, { opacity: 0, duration: 0.7, ease: "Power3.easeInOut" });
-  });
-};
+// exports.onClientEntry = () => {
+//   window.addEventListener("load", () => {
+//     const body = document.querySelector("body");
+//     body.className = document.body.className.replace(/\bno-js\b/, "");
+//     gsap.from(body, { opacity: 0, duration: 0.7, ease: "Power3.easeInOut" });
+//   });
+// };
