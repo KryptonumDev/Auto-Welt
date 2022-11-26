18d0abc80ce31fed502c8fefd6e7526359f675d1
Albert Strzyzewski <albert.strzyzewski@rb.com>
2022-11-26 16:56:25 +0100

    Naprawa błędów konsoli

--- src/components/GoogleMapsContact/GoogleMapsContact.js
+++ src/components/GoogleMapsContact/GoogleMapsContact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { GatsbyImage, getImage, withArtDirection } from "gatsby-plugin-image";
 
 import {
   StyledGoogleMapsContact,
@@ -23,10 +22,10 @@ const GoogleMapsContact = ({ mapData }) => {
       <StyledGoogleMapsWrapper>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4957.304738428757!2d19.12880645201214!3d51.592934314422706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471a4682803c6339%3A0xb1c9dd3559c7244f!2sPlac%2011%20Listopada%2027%2C%2098-100%20%C5%81ask!5e0!3m2!1sru!2spl!4v1669391567703!5m2!1sru!2spl"
-          style={{border: 'none'}}z
-          allowfullscreen=""
+          style={{border: 'none'}}
+          allowFullScreen=""
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade" />
+          referrerPolicy="no-referrer-when-downgrade" />
       </StyledGoogleMapsWrapper>
     </StyledGoogleMapsContact>
   );
