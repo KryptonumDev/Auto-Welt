98e20aedd6eadd78973d4bf2aa05a80fe8ae6b3c
CodeYouLike <strzyzewski.albertms@gmail.com>
2022-11-29 20:27:54 +0100

    Naprawa buga z fontem

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -38,5 +38,13 @@ exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
           crossOrigin="anonymous"
           key="interFont"
       />,
+      <link
+          rel="preload"
+          href="/fonts/NocturneSerif-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+          key="interFont"
+      />,
   ])
 }
