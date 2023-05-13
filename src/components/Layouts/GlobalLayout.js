0756c7ea79a17717d8fce51de52b0098c3888efb
kryptonumstudio <kryptonumstudio@gmail.com>
2023-05-13 17:00:47 +0200

    remove import cookies

--- src/components/Layouts/GlobalLayout.js
+++ src/components/Layouts/GlobalLayout.js
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import {
   StyledOverflowWrapper,
 } from "./StyledGlobalLayout";
-import Cookies from "../Cookies/Cookies";
+// import Cookies from "../Cookies/Cookies";
 
 const GlobalLayout = ({ location, children }) => {
   useEffect(() => {
